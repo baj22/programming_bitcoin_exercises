1d809be001e1ad6a25e6feb58dda3bf2cb1e85b3
Anthony Burke <anthonyburke@Anthonys-MacBook-Pro.local>
2019-07-19 20:54:47 -0400

    first two chapters

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -141,8 +141,8 @@ class Point:
     # end::source1[]
 
     def __ne__(self, other):
-        # this should be the inverse of the == operator
-        raise NotImplementedError
+        return self.x != other.x or self.y != other.y \
+            or self.a != other.a or self.b != other.b
 
     def __repr__(self):
         if self.x is None:
@@ -161,24 +161,19 @@ class Point:
         if other.x is None:  # <4>
             return self
         # end::source3[]
+        
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
 
-        # Case 1: self.x == other.x, self.y != other.y
-        # Result is point at infinity
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x3 = s**2 - self.x - other.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
 
-        # Case 2: self.x ≠ other.x
-        # Formula (x3,y3)==(x1,y1)+(x2,y2)
-        # s=(y2-y1)/(x2-x1)
-        # x3=s**2-x1-x2
-        # y3=s*(x1-x3)-y1
-
-        # Case 3: self == other
-        # Formula (x3,y3)=(x1,y1)+(x1,y1)
-        # s=(3*x1**2+a)/(2*y1)
-        # x3=s**2-2*x1
-        # y3=s*(x1-x3)-y1
-
-        raise NotImplementedError
-
+        s = (3 * self.x**2 + self.a) / (2 * self.y)
+        x3 = s**2 - 2 * self.x
+        return self.__class__(x3, s*(self.x-x3)-self.y, self.a, self.b)
 
 class PointTest(TestCase):
 
